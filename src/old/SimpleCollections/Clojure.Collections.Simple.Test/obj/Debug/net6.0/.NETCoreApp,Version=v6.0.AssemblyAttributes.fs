96d8d2fd954922c2bb3bfb225f31821eb8529d1f
David Miller <dmiller2718@gmail.com>
2023-01-08 18:01:55 -0500

    Fix url/baseurl in _config, remove inadvertent src inclusion

--- src/old/SimpleCollections/Clojure.Collections.Simple.Test/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v6.0", FrameworkDisplayName=".NET 6.0")>]
-                do ()
