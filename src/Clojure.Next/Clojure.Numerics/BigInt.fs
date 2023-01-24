8d621c0705f74f2f8da3932a042a1ee0f9321a8d
David Miller <dmiller2718@gmail.com>
2023-01-23 23:43:00 -0500

    Fix bug in BigInt.Equals

--- src/Clojure.Next/Clojure.Numerics/BigInt.fs
+++ src/Clojure.Next/Clojure.Numerics/BigInt.fs
@@ -35,7 +35,7 @@ type BigInt(lpart: int64, bipart: BigInteger option) =
     static member valueOf(n: int64) = BigInt(n, None)
 
     override this.Equals(o) : bool =
-        if o = this then
+        if Object.ReferenceEquals(o,this) then
             true
         else
             match o with
