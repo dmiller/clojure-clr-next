40279d57e50d21592cd5f60bd077ce0e6f9e6117
David Miller <dmiller2718@gmail.com>
2023-02-01 12:04:59 -0500

    Add AtomicReference and ArrayChunk

---
+++ src/Clojure.Next/Clojure.Collections/Atomics.fs
@@ -0,0 +1,20 @@
+﻿namespace Clojure.Collections
+
+open System.Threading
+
+type AtomicReference<'T when 'T : not struct >(r:'T) =
+
+    let mutable ref = r
+
+    new() = AtomicReference(Unchecked.defaultof<'T>)
+
+    member this.CompareAndSet(expect:'T,update:'T)  =
+        let oldVal = Interlocked.CompareExchange( &ref , expect, update)
+        obj.ReferenceEquals(oldVal,expect)
+
+    member this.Get() = ref
+
+    member this.GetAndSet(update:'T) = Interlocked.Exchange(&ref,update)
+
+    override _.ToString() = ref.ToString()
+
