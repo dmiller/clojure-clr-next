f15d485684e901a89a558f5089ca1583b4a4c41d
David Miller <dmiller2718@gmail.com>
2025-02-19 16:52:35 -0500

    Add comments, code cleanup for ArrayChunk, ChunkBuffer, and ChunkedCons

--- src/Clojure.Next/Clojure.Collections/Chunking.fs
+++ src/Clojure.Next/Clojure.Collections/Chunking.fs
@@ -2,92 +2,99 @@
 
 open System
 
+/// An IChunk based on an array.
 [<Sealed>]
-type ArrayChunk(arr:obj array,offset:int ,iend:int) =
-    
-    new(arr,offset) = ArrayChunk(arr,offset,arr.Length)
+type ArrayChunk(_arr: obj array, offset: int, _end: int) =
+
+    new(arr, offset) = ArrayChunk(arr, offset, arr.Length)
 
 
     interface Counted with
-        member _.count() = iend-offset
+        member _.count() = _end - offset
 
 
     interface Indexed with
-        member _.nth(i) = arr[offset+i]
-        member this.nth(i,nf) =
-            if 0 <= i && i < (this:>Counted).count() then  
-                (this:>Indexed).nth(i)
+        member _.nth(i) = _arr[offset + i]
+
+        member this.nth(i, nf) =
+            if 0 <= i && i < (this :> Counted).count () then
+                (this :> Indexed).nth (i)
             else
                 nf
 
     interface IChunk with
         member _.dropFirst() =
-            if offset = iend then
+            if offset = _end then
                 raise <| InvalidOperationException("dropFirst of empty chunk")
             else
-                ArrayChunk(arr,offset+1,iend) 
+                ArrayChunk(_arr, offset + 1, _end)
 
-        member _.reduce(f,start) =
-            let ret = f.invoke(start,arr[offset])
-            let rec loop (ret:obj) idx =
-                match ret with  
+        member _.reduce(f, start) =
+            let ret = f.invoke (start, _arr[offset])
+
+            let rec loop (ret: obj) idx =
+                match ret with
                 | :? Reduced -> ret
-                | _ when idx >= iend -> ret
-                | _ -> loop (f.invoke(ret,arr[idx])) (idx+1)
-            loop ret (offset+1)
+                | _ when idx >= _end -> ret
+                | _ -> loop (f.invoke (ret, _arr[idx])) (idx + 1)
+
+            loop ret (offset + 1)
 
 
-
+/// Build a chunk in an array given capacity, and dliver it as an IChunk.
 [<Sealed>]
-type ChunkBuffer(capacity:int) =
+type ChunkBuffer(capacity: int) =
 
-    let mutable buffer : obj array = Array.zeroCreate capacity
-    let mutable cnt : int = 0
+    let mutable _buffer: obj array = Array.zeroCreate capacity
+    let mutable _cnt: int = 0
 
     interface Counted with
-        member _.count() = cnt
+        member _.count() = _cnt
 
-    member _.add(o:obj) = 
-        buffer[cnt] <- 0
-        cnt <- cnt+1
+    member _.add(o: obj) =
+        _buffer[_cnt] <- 0
+        _cnt <- _cnt + 1
 
     member _.chunk() : IChunk =
-        let ret = ArrayChunk(buffer,0,cnt)
-        buffer <- null
+        let ret = ArrayChunk(_buffer, 0, _cnt)
+        _buffer <- null
         ret
 
-type ChunkedCons(meta:IPersistentMap, chunk:IChunk, more:ISeq) =
+/// Provide consing a chunk on the front of an ISeq.
+type ChunkedCons(meta: IPersistentMap, _chunk: IChunk, _more: ISeq) =
     inherit ASeq(meta)
 
-    new(chunk,more) = ChunkedCons(null,chunk,more)
+    new(chunk, more) = ChunkedCons(null, chunk, more)
 
-    interface IObj with 
+    interface IObj with
         override this.withMeta(m) =
             if LanguagePrimitives.PhysicalEquality m meta then
                 this
             else
-                ChunkedCons(m,chunk,more)
+                ChunkedCons(m, _chunk, _more)
 
     interface ISeq with
-        override _.first() = chunk.nth(0)
+        override _.first() = _chunk.nth (0)
+
         override this.next() =
-            if chunk.count() > 1 then
-                ChunkedCons(chunk.dropFirst(),more)
-            else 
-                (this:>IChunkedSeq).chunkedNext()
+            if _chunk.count () > 1 then
+                ChunkedCons(_chunk.dropFirst (), _more)
+            else
+                (this :> IChunkedSeq).chunkedNext ()
+
         override this.more() =
-            if chunk.count() > 1 then
-                ChunkedCons(chunk.dropFirst(),more)
-            elif isNull more then
+            if _chunk.count () > 1 then
+                ChunkedCons(_chunk.dropFirst (), _more)
+            elif isNull _more then
                 PersistentList.Empty
             else
-                more
+                _more
 
     interface IChunkedSeq with
-        member _.chunkedFirst() = chunk
-        member this.chunkedNext() = (this:>IChunkedSeq).chunkedMore().seq()
+        member _.chunkedFirst() = _chunk
+
+        member this.chunkedNext() =
+            (this :> IChunkedSeq).chunkedMore().seq ()
+
         member _.chunkedMore() =
-            if isNull more then
-                PersistentList.Empty
-            else 
-                more
+            if isNull _more then PersistentList.Empty else _more
